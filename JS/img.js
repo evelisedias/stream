09ff6537c7d73bbab892edef5d506ffdc05b97ea
evelisedias <evelisemacieldias@outlook.com>
2021-12-31 00:27:53 -0300

    iniciando

---
+++ JS/img.js
@@ -0,0 +1,114 @@
+// Lista de imagens e suas variaveis
+
+    let imagemAchismo;
+    let imagemEuFicoLoko;
+    let imagemFlow;
+    let imagemLez;
+    let imagemMini;
+    let imagemNerdCast;
+    let imagemPodepah;
+    let imagemPretinho;
+    let imagemCafedaManha;
+    let imagemCafecomadm;
+    let imagemDurmacomEssa;
+    let imagemFerrisShow;
+    let imagemJJ;
+    let imagemPoupecast;
+    let imagemPrimoCast;
+    let imagemPrincipal;
+    let imagemValor;
+    let imagemInglesdoZero;
+    let imagemFrench;
+    let imagemGermany;
+    let imagemIrish;
+    let imagemJapones;
+    let imagemRussian;
+    let imagemSpanish;
+    let imagemTurkish;
+    let imagemValor;
+    let imagemBBC;
+    let imagemBraincast;
+    let imagemDiretoaoPonto;
+    let imagemOAssunto;
+    let imagemResumao;
+    let imagemSaudeSemTabu;
+    let imagemTR;
+    let imagemCafepsi;
+    let imagemDetetives;
+    let imagemDrauzio;
+    let imagemHeadspace;
+    let imagemIronberg;
+    let imagemNozdanutri;
+    let imagemZencast;
+
+    let imagemEntretenimento
+    let imagemFinancas
+    let imagemIdiomas
+    let imagemInformativos
+    let imagemSaude
+
+//Carregando imagens
+function preload(){
+
+// imagens para carrosel de entretenimento
+
+    imagemAchismo = loadimage("entretenimento/achismo.jpg");
+    imagemEuFicoLoko = loadimage("entretenimento/euficoloko.jpg");
+    imagemFlow = loadimage("entretenimento/Flow.jpg");
+    imagemLez = loadimage("entretenimento/lez.jpg");
+    imagemMini = loadimage("entretenimento/mini.jpg");
+    imagemNerdCast = loadimage("entretenimento/nerdcast.jpg");
+    imagemPodepah= loadimage("entretenimento/podepah.jpg");
+    imagemPretinho = loadimage("entretenimento/pretinho.jpg");
+
+// imagens para carrosel de Finanças e política
+
+    imagemCafedaManha = loadimage("entretenimento/cafe-da-manha.jpg");
+    imagemCafecomadm = loadimage("entretenimento/cadecomadm.jfif");
+    imagemDurmacomEssa = loadimage("entretenimento/durma-com-essa.png");
+    imagemFerrisShow = loadimage("entretenimento/Ferrisshoe.jpeg");
+    imagemJJ = loadimage("entretenimento/jj-podcast.jfif");
+    imagemPoupecast = loadimage("entretenimento/poupecast.jfif");
+    imagemPrimoCast= loadimage("entretenimento/Primocast.jpg");
+    imagemValor = loadimage("entretenimento/Valor.jpg");
+
+// imagens para carrosel de Idiomas
+
+    imagemInglesdoZero = loadimage("entretenimento/en-dozero,png");
+    imagemFrench = loadimage("entretenimento/French.jpg");
+    imagemGermany = loadimage("entretenimento/Germany.jpg");
+    imagemIrish = loadimage("entretenimento/irish.png");
+    imagemJapones = loadimage("entretenimento/japones.jpg");
+    imagemRussian = loadimage("entretenimento/russian.jpg");
+    imagemSpanish= loadimage("entretenimento/spanish.jpg");
+    imagemTurkish = loadimage("entretenimento/turkish.jpg");
+
+// imagens para carrosel de Informativos
+
+    imagemBBC = loadimage("entretenimento/bbc.jpg");
+    imagemBraincast = loadimage("entretenimento/braincast.png");
+    imagemDiretoaoPonto = loadimage("entretenimento/Diretoaoponto.jpg");
+    imagemOAssunto= loadimage("entretenimento/oassunto.jfif");
+    imagemResumao = loadimage("entretenimento/resumao.jpg");
+    imagemSaudeSemTabu = loadimage("entretenimento/saude sem tabu.png");
+    imagemTR= loadimage("entretenimento/tr.png");
+
+// imagens para carrosel de Saúde
+
+        imagemCafepsi = loadimage("entretenimento/cafepsi.jpg");
+        imagemDetetives = loadimage("entretenimento/detetives-da-saude.png");
+        imagemDrauzio = loadimage("entretenimento/drauziocast.png");
+        imagemHeadspace= loadimage("entretenimento/hedspace.png");
+        imagemIronberg = loadimage("entretenimento/ironberg.jfif");
+        imagemNozdanutri = loadimage("entretenimento/nozdanutri.jfif");
+        imagemZencast= loadimage("entretenimento/zencast.png");
+
+}
+
+imagemEntretenimento = [imagemAchismo, imagemEuFicoLoko, imagemFlow, imagemLez, imagemMini, imagemNerdCast, imagemPodepah, imagemPretinho]
+imagemFinancas = [imagemCafedaManha, imagemCafecomadm, imagemDurmacomEssa, imagemFerrisShow, imagemFerrisShow, imagemJJ, imagemPoupecast, imagemPrimoCast, imagemValor]
+imagemIdiomas = [imagemInglesdoZero, imagemFrench, imagemGermany, imagemIrish, imagemJapones, imagemRussian, imagemSpanish, imagemTurkish]
+imagemInformativos = [imagemBBC, imagemBraincast, imagemDiretoaoPonto, imagemOAssunto, imagemResumao, imagemSaudeSemTabu, imagemTR]
+imagemSaude = [cafepsi, imagemDetetives, imagemDrauzio, imagemHeadspace, imagemIronberg, imagemNozdanutri, imagemZencast]
+
+
---
+++ README.md
@@ -0,0 +1,16 @@
+## Hey peeps
+
+
+Este projeto trata-se de uma ideia própria para inclusão na prática as linguagens em que tenho aprendido.
+
+
+* #### Ideia:
+
+    - Criar uma plataforma de podcasts em que o usuário possa ouvir sem sair da página.
+
+
+* #### Itens para incluir:
+    
+    - Criar um player que estarta ao clicar sobre a imagem do podcast desejado;
+    - Demonstrar a lista de episodios disponíveis;
+
